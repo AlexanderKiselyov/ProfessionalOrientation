d96b3ae648a9bc5a30304612446e9a7428792e12
izhko <izhko.gosha@gmail.com>
2019-07-07 01:10:05 +0300

    Добавил возможность менять ориентацию телефона, но нужно поменять картинку для портретного вида!!!!

--- app/src/main/java/com/icst/professionalOrientation/MainActivity.java
+++ app/src/main/java/com/icst/professionalOrientation/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         recom_prof = new int[3];
         leftButton = -1;
